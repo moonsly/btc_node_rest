31862d5e1f92943d3c3a299608d03e9b1215eded
Alex Kazakov <moonsly@gmail.com>
2018-07-03 21:17:02 +0300

    Update settings_local.py

--- btc_node_rest/settings_local.py
+++ btc_node_rest/settings_local.py
@@ -1,17 +1,17 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btc_node',
-        'USER': 'septemex',
-        'PASSWORD': 'septemex',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
 
 CONSTANCE_CONFIG = {
-    'NODE_LOGIN': ('cashberycoinrpc', 'node login'),
-    'NODE_PASSWORD': ('3yXvc24JawmTncHduoA6er9MKWW7a4fu19NyRP1RtwPm', 'node password'),
-    'NODE_IP': ('5.9.104.23', 'node IP'),
-    'NODE_PORT': ('8332', 'node port'),
+    'NODE_LOGIN': ('', 'node login'),
+    'NODE_PASSWORD': ('', 'node password'),
+    'NODE_IP': ('', 'node IP'),
+    'NODE_PORT': ('', 'node port'),
 }
